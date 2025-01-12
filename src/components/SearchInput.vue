aac74661318587300b3ef027c9b5416a473997f6
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2025-01-12 00:47:59 -0300

    feat: add search input

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+const loaded = ref(false);
+const loading = ref(false);
+
+const onClick = () => {
+  loading.value = true;
+  setTimeout(() => {
+    loading.value = false;
+    loaded.value = true;
+  }, 2000);
+};
+</script>
+
+<template>
+  <v-text-field :loading="loading" class="mx-auto my-10" placeholder="Busque o vídeo pelo título"
+    append-inner-icon="mdi-magnify" density="comfortable" variant="solo" hide-details single-line
+    persistent-placeholder :width="500" @click:append-inner="onClick" />
+</template>
