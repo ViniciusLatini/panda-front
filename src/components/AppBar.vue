080a39046ba97bbc841a1de72bbf45de7a67803a
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2025-01-12 00:47:24 -0300

    feat: add appBar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-app-bar :elevation="5">
+    <template #prepend>
+      <v-img src="../assets/logo-light.svg" :height="70" :width="200" />
+    </template>
+
+    <template #append>
+      <v-btn icon="mdi-logout" />
+    </template>
+  </v-app-bar>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,11 @@
+<script setup>
+import AppBar from '@/components/AppBar.vue';
+
+</script>
+
 <template>
+  <AppBar />
   <v-main>
     <router-view />
   </v-main>
-
-  <AppFooter />
 </template>
-
-<script setup>
-  //
-</script>
