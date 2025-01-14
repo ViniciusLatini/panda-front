30d7a691d0f53553bdb2f7d57fe765eb2909623f
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2025-01-14 11:47:30 -0300

    feat: create getVideoByIdService

--- src/services/videos.js
+++ src/services/videos.js
@@ -4,3 +4,8 @@ export async function getVideosService() {
   const { data } = await axiosClient.get("/videos");
   return data.videos;
 }
+
+export async function getVideoByIdService(id) {
+  const { data } = await axiosClient.get(`/videos/${id}`);
+  return data;
+}
