f4c9ad47d6c5978d556dbc328a315a0f3633164b
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2025-01-14 02:04:21 -0300

    feat: send token authorization

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,4 +4,11 @@ const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_VUE_APP_API_URL
 })
 
+axiosClient.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token)
+    config.headers.Authorization = `Bearer ${token}`;
+  return config
+})
+
 export default axiosClient;
