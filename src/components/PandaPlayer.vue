1b5eb59ffc3036fe5023cbe4a7fb53fda5318eaa
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2025-01-14 11:48:04 -0300

    feat: create PandaPlayer component

---
+++ src/components/PandaPlayer.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const { videoPlayer } = defineProps(['videoPlayer']);
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-lg">
+      <div style="position:relative;padding-top:56.25%;">
+        
+        <iframe id="panda-player"
+          :src="videoPlayer"
+          style="border:none;position:absolute;top:0;height:100%;width:100%; border-radius: 10px;"
+          allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture" allowfullscreen="true" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
